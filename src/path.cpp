18acca551ef587e3bb0a84ba5fa0dd173ab41eee
NilpotentHX3 <antonin.riffard@free.fr>
2015-12-13 06:10:42 +0100

    Correction apportée à Path
    Conflicts:
    	src/path.cpp
    	src/path.hpp

--- src/path.cpp
+++ src/path.cpp
@@ -15,14 +15,3 @@ void Path::addBranch(Node::ID node, Branch::ID branch, float len)
 Branch::ID Path::getBranchID(int n) {
   return mPath[n].second;
 }
-
-/*float Path::length()
-{
-    float length = 0.f;
-    for (auto pere : mPath)
-    {
-        Branch::ID branch = pere.sd;
-        length += branch.getLength();
-    }
-    return length;
-}*/
